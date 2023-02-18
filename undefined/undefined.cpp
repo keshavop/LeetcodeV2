0964585c8bb713ac6ae5add61f96f37c724980d4
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2023-02-18 20:30:06 +0530

    Time: None, Memory: None',   langs: [
      ],
      runtime: '2',
      memory: '5984000',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'None - LeetHub

--- undefined/undefined.cpp
+++ undefined/undefined.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    int minMaxDifference(int num) {
 
-    vector<int> separateDigits(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            string digit = to_string(nums[i]);
-            for(int j =0;j<digit.length();j++){
-                ans.push_back(digit[j]-'0');
+std::string num_str = std::to_string(num);
+    int max_val = num;
+    int min_val = num;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            std::string new_num_str = num_str;
+            std::replace(new_num_str.begin(), new_num_str.end(), '0' + i, '0' + j);
+            int new_num = std::stoi(new_num_str);
+            if (new_num > max_val) {
+                max_val = new_num;
+            }
+            if (new_num < min_val && (j > 0 || i > 0)) {
+                min_val = new_num;
             }
         }
-    return ans;
     }
-};
+    return max_val - min_val;
+    }
+};//a
