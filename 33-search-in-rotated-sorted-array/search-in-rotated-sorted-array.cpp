db196e55af6581e85c749fe3e83a3d1c6b8612fb
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2023-08-11 17:13:11 +0530

    Time: 7 ms (24.32%) | Memory: 11.1 MB (18.26%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,41 +1,10 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        
-        int start = 0;
-        int n = arr.size();
-        int end = n-1;
-
-        while(start <= end){
-
-            int mid = (start + end)/2;
-
-            // cout<<arr[mid]<<"\n";
-
-            if(arr[mid] == target){
-                return mid;
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
             }
-
-            else if(arr[start] <= arr[mid]){  //left side sorted
-                if(arr[start] <= target && arr[mid] >= target){
-                    // cout<<"no"<<"\n";
-                    end = mid -1;
-                }
-                else{
-                    // cout<<"yes"<<"\n";
-                    start = mid + 1;
-                }
-            }
-
-            else if(arr[end] >= arr[mid]){
-                if(arr[mid] <= target && arr[end] >= target){
-                    start = mid + 1;
-                }
-                else{
-                    end = mid - 1;
-                }
-            }
-
         }
         return -1;
     }
